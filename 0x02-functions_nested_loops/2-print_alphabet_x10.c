1ef443487b338112aa9575b32f86c51ca41f4b4e
Nyandapatrick <patricknyanda77@gmail.com>
2022-07-06 11:58:51 -0700

    hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
+
 /**
- * main - Entry point
- *
- * A function that prints 10 times the alphabet, in lowercase,
+ * print_alphabet_x10 ->  prints the alphabet in lowercase
  */
 void print_alphabet_x10(void)
 {
