6d0e4e5e9470150a848fc166a6f6a7350be73393
Nyandapatrick <patricknyanda77@gmail.com>
2022-07-08 10:35:48 -0700

    prints numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,8 +5,10 @@
  */
 void print_numbers(void)
 {
-	int x;
-	for (x = 0; x < 10; x++)
-		_putchar(x + '0');
-	_putchar('\n');
+int x;
+for (x = 0; x < 10; x++)
+{
+	_putchar('0');
+}
+_putchar('\n');
 }
