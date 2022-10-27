9d7e7e03a15151e430728ddbe6c5592aa6f20d3a
Nyandapatrick <patricknyanda77@gmail.com>
2022-10-27 15:29:52 -0700

    program prints a[2] = 98

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Adding 0ne code so the Program prints a[2] = 98 followed by new line
+ * main - A program prints a[2] = 98, followed by a new line
  *
  * Return: Always (0)
  */
@@ -12,9 +12,8 @@ int main(void)
 	int a[5];
 	int *p;
 
-	a[2] = 98;
+	a[2] = 1024;
 	p = &n;
-
 	/*
 	 * write your line of code here...
 	 * Remember:
