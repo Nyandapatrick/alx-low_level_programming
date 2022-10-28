b23e7fef7c06150cc8bd3e9ba063a22cceff62dd
Nyandapatrick <patricknyanda77@gmail.com>
2022-10-28 07:21:25 -0700

    prints a[2] = 98

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - A program prints a[2] = 98, followed by a new line
+ * main - Adding 0ne code so the Program prints a[2] = 98 followed by new line
  *
  * Return: Always (0)
  */
@@ -9,21 +9,14 @@
 int main(void)
 {
 	int n;
-	int a[2];
+	int a[5];
 	int *p;
 
 	a[2] = 98;
 	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
-	*(p + 2) = 98;
-	/* ...so that this prints 98\n */
+	*(p + 5) = 98;
+
 	printf("a[2] = %d\n", a[2]);
+
 	return (0);
 }
--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,27 +1,43 @@
-#include "main.h"
-
+#include "holberton.h"
+#include <stdio.h>
 /**
- * rot13 - Encodes a string in rot13
- * @s: string to be encoded
- * Return: the resulting strring
+ * infinite_add - Adds two numbers
+ * @n1: first input string
+ * @n2: second input string
+ * @r: pointer to buffer where result is stored
+ * @size_r: requested size for the buffer
+ * Return: pointer to buffer where result is stored
  */
-char *rot13(char *s)
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j;
+	/* i = iterator for n1 and n2; j = iterator for r; n = carry over number */
+	int i, j, n;
 
-	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-	for (i = 0; s[i] != '\0'; i++)
+	i = j = n = 0;
+	/* if r[0] >= 10, set value to 1 & increase buffer size by 1*/
+	if ((n1[0] - '0') + (n2[0] - '0') >= 10)
 	{
-		for (j = 0; a[j] != '\0'; j++)
-		{
-			if (s[i] == a[j])
-			{
-				s[i] = b[j];
-				break;
-			}
-		}
+		r[0] = 1 + '0';
+		j = 1;
 	}
-	return (s);
+	while (i < size_r && (n1[i] != '\0' || n2[i] != '\0' || r[j] != '\0'))
+	{
+		if ((n1[i + 1] - '0') + (n2[i + 1] - '0') >= 10)
+			n = 1;
+		else
+			n = 0;
+		r[j] = (n1[i] - '0') + (n2[i] - '0') + n;
+		r[j] = r[j] % 10 + '0';
+		/**
+		 * printf("i: %d, n1: %d, n2: %d, j: %d, r: %d\n", i, n1[i] - '0', n2[i] - '0', j, r[j]- '0')
+		 *
+		 */
+		i++;
+		j++;
+		if (n1[i] == '\0' || n2[i] == '\0')
+			r[j] = '\0';
+	}
+	r[j] = '\0';
+	return (r);
 }
